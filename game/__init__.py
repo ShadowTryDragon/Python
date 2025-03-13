37dc2ab48dc98976dcd8e9ecb84a0b4a0d00adc8
ShadowTryDragon <90843434+ShadowTryDragon@users.noreply.github.com>
2025-03-13 15:04:12 +0100

    Create __init__.py

---
+++ game/__init__.py
@@ -0,0 +1,9 @@
+from .settings import Settings
+from .apple import Apple, FakeApple, SuperApple, SugarApple, ReverseApple, MegaApple
+from .database import *
+from .obstacles import HunterObstacle
+from .obstacles import Obstacle
+from .snake import Snake
+from .bot import BotSnake
+from .gamecolors import GameColors
+from .classic import ClassicSnakeGame
