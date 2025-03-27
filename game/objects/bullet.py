c4c81ad796cb42e1a1203fc259cdd60d7d5aea3d
ShadowTryDragon <90843434+ShadowTryDragon@users.noreply.github.com>
2025-03-27 14:45:08 +0100

    Update bullet.py
    kleine verbesserungen an den Bullets

--- game/objects/bullet.py
+++ game/objects/bullet.py
@@ -1,13 +1,16 @@
 import pygame
+
 from game.setting.settings import Settings
 
+
 class Bullet:
-    def __init__(self, position, direction):
+    def __init__(self, position, direction, owner):
         self.__position = position
         self.__direction = direction
         self.__speed = 10
         self.__image = pygame.image.load("game/icons/sprites/bullet.png").convert_alpha()
-        self.__image = pygame.transform.scale(self.__image, (Settings.grid_size, Settings.grid_size))
+        self.__image = pygame.transform.scale(self.__image, (Settings.grid_size // 2, Settings.grid_size // 2))
+        self.owner = owner  # 🔥 Speichert, wer die Kugel abgeschossen hat (Spieler oder Feind)
 
     def move(self):
         """Bewegt das Geschoss in die gewählte Richtung."""
